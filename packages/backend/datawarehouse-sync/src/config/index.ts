8bc3a2fb825e0bce6e4e564626b59b20c667e618
gokaju <gokaju23@gmail.com>
2023-08-28 23:11:19 -0500

    --

---
+++ packages/backend/datawarehouse-sync/src/config/index.ts
@@ -0,0 +1,3 @@
+export default class Config {
+  static portNumber: number = Number(process.env.PORT || '3001')
+}
---
+++ packages/backend/datawarehouse-sync/src/index.ts
@@ -0,0 +1,27 @@
+import { createServer } from 'http'
+import { Server, Socket } from 'socket.io'
+
+import { config } from 'dotenv'
+import cnf from './config'
+config()
+const httpServer = createServer()
+const io = new Server(httpServer, { cors: { origin: '*' } })
+
+io.of('signature-pad').on('connection', (socket: Socket) => {
+  console.log('a user connected' + socket.id)
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
+  socket.on('join', function (room: string) {
+    socket.join(room)
+    socket.emit('joined', room)
+    io.to(room).emit('message', { room: room, id: socket.id })
+  })
+  socket.on('message', function (message: any) {
+    socket.broadcast.to(message.room).emit('message', message)
+    io.to(message.room).emit('message', message)
+  })
+})
+
+httpServer.listen(cnf.portNumber)
+console.log(`listening on port ${cnf.portNumber}`)
