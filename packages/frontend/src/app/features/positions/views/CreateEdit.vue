3e2eb0a71e6cc4798a19f01d823ad293ff118cdf
Deiby Gomez <gokaju23@gmail.com>
2024-02-07 22:52:25 -0500

    Refactor CreateEdit.vue component add isDoctor field

--- packages/frontend/src/app/features/positions/views/CreateEdit.vue
+++ packages/frontend/src/app/features/positions/views/CreateEdit.vue
@@ -1,101 +1,110 @@
 <script setup lang="ts">
-import { onBeforeMount, ref } from "vue";
-import { useRoute, useRouter } from "vue-router";
-import { Form, DynamicForm } from "../../dynamic-form";
-import { useNotificationsStore } from "@/store/notifications";
+import { onBeforeMount, ref } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+import { Form, DynamicForm } from '../../dynamic-form'
+import { useNotificationsStore } from '@/store/notifications'
 
-import { create, getEntity, edit } from "../services";
+import { create, getEntity, edit } from '../services'
 
-const notifications = useNotificationsStore();
-const moduleName = "Cargo";
-const modulePath = "positions";
+const notifications = useNotificationsStore()
+const moduleName = 'Cargo'
+const modulePath = 'positions'
 
-const route = useRoute();
-const router = useRouter();
+const route = useRoute()
+const router = useRouter()
 
-let model = {};
-const loading = ref(false);
+let model = {}
+const loading = ref(false)
 
 const form: Form = {
   entity: modulePath,
   tabs: [
     {
-      name: "Datos",
+      name: 'Datos',
       groups: [
         {
-          name: "Información basica",
-          description: "Información basica del cargo",
+          name: 'Información basica',
+          description: 'Información basica del cargo',
           fields: [
             {
-              name: "name",
-              label: "Nombre del Cargo",
-              type: "text",
+              name: 'name',
+              label: 'Nombre del Cargo',
+              type: 'text',
               props: {
-                placeholder: "Nombre del Cargo",
-                class: "lg:col-span-6 xl:col-span-6",
+                placeholder: 'Nombre del Cargo',
+                class: 'lg:col-span-4 xl:col-span-4',
                 required: true,
               },
-              rules: ["required", "upper", "minlength:3", "maxlength:50"],
+              rules: ['required', 'upper', 'minlength:3', 'maxlength:50'],
+            },
+            {
+              name: 'isDoctor',
+              label: 'Es Medico',
+              type: 'check',
+              props: {
+                placeholder: 'Codigo del Cargo',
+              },
+              rules: [],
             },
           ],
         },
       ],
     },
   ],
-};
+}
 
 const onSubmit = async (data: any) => {
-  console.log("Submit", data);
+  console.log('Submit', data)
 
   if (route.params.id === undefined) {
-    console.log("Create");
+    console.log('Create')
     if (await create(data)) {
       notifications.addNotification({
-        type: "success",
+        type: 'success',
         title: `${moduleName} creado`,
         text: `El ${moduleName} se ha creado correctamente`,
-      });
-      router.push({ name: `${modulePath}.list` });
+      })
+      router.push({ name: `${modulePath}.list` })
     } else {
       notifications.addNotification({
-        type: "error",
-        title: "Error",
+        type: 'error',
+        title: 'Error',
         text: `No se ha podido crear el ${moduleName}`,
-      });
+      })
     }
   } else {
     if (await edit(route.params.id as string, data)) {
       notifications.addNotification({
-        type: "success",
+        type: 'success',
         title: `${moduleName} actualizado`,
         text: `El ${moduleName} se ha actualizado correctamente`,
-      });
-      router.push({ name: `${modulePath}.list` });
+      })
+      router.push({ name: `${modulePath}.list` })
     } else {
       notifications.addNotification({
-        type: "error",
-        title: "Error",
+        type: 'error',
+        title: 'Error',
         text: `No se ha podido actualizar el ${moduleName}`,
-      });
+      })
     }
   }
-};
+}
 
 const back = () => {
-  console.log("Back");
-  router.push({ name: `${modulePath}.list` });
-};
+  console.log('Back')
+  router.push({ name: `${modulePath}.list` })
+}
 
 onBeforeMount(async () => {
-  loading.value = true;
+  loading.value = true
 
-  console.log("Mounted", route.params.id);
+  console.log('Mounted', route.params.id)
   if (route.params.id) {
-    model = await getEntity(route.params.id as string);
-    console.log("Model", model);
+    model = await getEntity(route.params.id as string)
+    console.log('Model', model)
   }
-  loading.value = false;
-});
+  loading.value = false
+})
 </script>
 
 <template>
@@ -103,7 +112,7 @@ onBeforeMount(async () => {
     <div class="bg-gray-50 pb-4">
       <div class="leading-4 pt-responsive">
         <p class="text-3xl font-semibold text-shadow text-blue-900">
-          {{ route.params.id == undefined ? "Crear" : "Editar" }}
+          {{ route.params.id == undefined ? 'Crear' : 'Editar' }}
           {{ moduleName }}
         </p>
       </div>
