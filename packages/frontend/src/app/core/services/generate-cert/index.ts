beb1e3ec4c9d24c63732c310fcbd63d5e54de2a7
Deiby Gomez <gokaju23@gmail.com>
2024-02-08 00:52:17 -0500

    Add reverse proxy for /api/certificates/

--- packages/frontend/src/app/core/services/generate-cert/index.ts
+++ packages/frontend/src/app/core/services/generate-cert/index.ts
@@ -1,12 +1,9 @@
 import axios from 'axios'
 
 export async function generateInformedConsent(orderId: string) {
-  const certificate = await axios.post(
-    'http://localhost:3005/api/certificates/',
-    {
-      order: orderId,
-      code: 'INFORMED-CONSENT',
-    }
-  )
+  const certificate = await axios.post('http://localhost/api/certificates/', {
+    order: orderId,
+    code: 'INFORMED-CONSENT',
+  })
   return certificate.data
 }
