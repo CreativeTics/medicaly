61c80fa649a1c5549d71e37237f488e6bee35b1e
Deiby Gomez <gokaju23@gmail.com>
2025-01-14 11:07:28 -0500

    Update METABASE URL configuration to use METABASE_SITE_URL environment variable

--- packages/backend/api-gateway/src/config/index.ts
+++ packages/backend/api-gateway/src/config/index.ts
@@ -17,7 +17,7 @@ export default (): IConfig => ({
     PASSWORD: process.env.COUCHDB_PASSWORD || '4dm1n-p4ssw0rd!!',
   },
   METABASE: {
-    URL: process.env.METABASE_URL || 'http://localhost:3000',
+    URL: process.env.METABASE_SITE_URL || 'http://localhost:3000',
     SECRET_KEY: process.env.METABASE_SECRET_KEY || 'NO-SECRET-KEY',
   },
 })
