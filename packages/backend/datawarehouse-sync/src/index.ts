784dfaff64a9b84092e555f534d3465ad0fa1ce8
Deiby Gomez <gokaju23@gmail.com>
2025-03-16 15:45:02 -0500

    Refactor cron job to await asynchronous service calls for improved execution flow

--- packages/backend/datawarehouse-sync/src/index.ts
+++ packages/backend/datawarehouse-sync/src/index.ts
@@ -1,18 +1,18 @@
-import { schedule } from 'node-cron'
 import { config } from 'dotenv'
-config()
+import { schedule } from 'node-cron'
 import constants from './config'
 import { syncGeneral } from './services/sync-general'
 import { syncMedical } from './services/sync-medical'
 import { syncMedicalAnnotations } from './services/sync-medical-annotations'
+config()
 
 const CronJobExpression = constants().CRON_EXPRESSION
 
 schedule(CronJobExpression, async () => {
   console.log(`Cron job started ... at ${CronJobExpression}`)
-  syncGeneral()
-  syncMedical()
-  syncMedicalAnnotations()
+  await syncGeneral()
+  await syncMedicalAnnotations()
+  await syncMedical()
 })
 
 console.log(`Cron job started ... at ${CronJobExpression}`)
