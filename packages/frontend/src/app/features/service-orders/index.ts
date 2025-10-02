4512c619d6cc87079bbe4e4389d6198c1c68cdd7
Deiby Gomez <gokaju23@gmail.com>
2025-10-02 08:28:33 -0500

    feat(ServiceOrders): add edit functionality and improve route handling for service orders

--- packages/frontend/src/app/features/service-orders/index.ts
+++ packages/frontend/src/app/features/service-orders/index.ts
@@ -1,42 +1,43 @@
-import ServiceOrdersList from "./views/List.vue";
-import ServiceOrdersCreate from "./views/BulkCreate.vue";
-import ServiceOrdersDetail from "./views/Detail.vue";
+import ServiceOrdersCreate from './views/BulkCreate.vue'
+import ServiceOrdersDetail from './views/Detail.vue'
+import ServiceEdit from './views/Edit.vue'
+import ServiceOrdersList from './views/List.vue'
 
 export const serviceOrdersRoutes = [
   {
-    path: "/service-orders",
-    name: "service-orders.list",
+    path: '/service-orders',
+    name: 'service-orders.list',
     component: ServiceOrdersList,
     meta: {
       auth: true,
-      layout: "home",
+      layout: 'home',
     },
   },
   {
-    path: "/service-orders/create",
-    name: "service-orders.create",
+    path: '/service-orders/create',
+    name: 'service-orders.create',
     component: ServiceOrdersCreate,
     meta: {
       auth: true,
-      layout: "home",
+      layout: 'home',
     },
   },
   {
-    path: "/service-orders/view/:id",
-    name: "service-orders.view",
+    path: '/service-orders/view/:id',
+    name: 'service-orders.view',
     component: ServiceOrdersDetail,
     meta: {
       auth: true,
-      layout: "home",
+      layout: 'home',
     },
   },
   {
-    path: "/service-orders/edit/:id",
-    name: "service-orders.edit",
-    component: ServiceOrdersCreate,
+    path: '/service-orders/:id',
+    name: 'service-orders.edit',
+    component: ServiceEdit,
     meta: {
       auth: true,
-      layout: "home",
+      layout: 'home',
     },
   },
-];
+]
--- packages/frontend/src/app/features/service-orders/views/Edit.vue
+++ packages/frontend/src/app/features/service-orders/views/Edit.vue
@@ -1,69 +1,34 @@
 <script setup lang="ts">
-import { useImageFile } from '@/app/core/composable/useImageFile'
 import { onMounted, onUnmounted, ref } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 
 import { OrderStatus as OrderStatusEnum } from '@/app/core/types/order-status'
 import { useNotificationsStore } from '@/store/notifications'
 import OrderStatus from '../components/OrderStatus.vue'
-import {
-  downloadExamCertificate,
-  getAnnotationUrl,
-  getOrder,
-} from '../services'
-import { getPatient } from '../services/patients'
+import { getOrder } from '../services'
 
 import { DBtn } from '@components/basic'
 import {
-  ArrowRightIcon,
   FileAttachment01Icon as ExamIcon,
-  Loading01Icon,
+  RefreshCcw01Icon,
+  Trash01Icon,
   XIcon,
 } from '@components/basic/icons'
+import Popper from 'vue3-popper'
+import OrderCycle from '../components/OrderCycle.vue'
 
 const notifications = useNotificationsStore()
 const route = useRoute()
 const router = useRouter()
-const photo = useImageFile()
 
 const order = ref<any>({})
 const loading = ref(false)
 
 // modal
 const modalActive = ref(false)
-const selectedAnnotationUrl = ref('')
-const handleShowAnnotation = async (serviceId: string, exam: any) => {
-  selectedAnnotationUrl.value = await getAnnotationUrl(
-    order.value.id,
-    serviceId,
-    exam.id
-  )
-  modalActive.value = true
-}
+
 const closeModal = () => {
   modalActive.value = false
-  selectedAnnotationUrl.value = ''
-}
-const handleDownloadExamCertificate = async (
-  serviceId: string,
-  exam: {
-    id: string
-    loading: boolean
-  }
-) => {
-  exam.loading = true
-  try {
-    const examId = exam.id
-    // Descargar el examen
-    await downloadExamCertificate(order.value.id, serviceId, examId)
-  } catch (error: any) {
-    notifications.addNotification({
-      type: 'error',
-      title: 'No se puede descargar el examen',
-      text: error.message,
-    })
-  }
-  exam.loading = false
 }
 
 const back = () => {
@@ -74,8 +39,6 @@ onMounted(async () => {
   if (route.params.id) {
     loading.value = true
     order.value = await getOrder(route.params.id.toString())
-    const patientData = await getPatient(order.value.patientDataId)
-    photo.loadImageFromId(patientData.photoId)
     loading.value = false
   }
 
@@ -104,8 +67,44 @@ onUnmounted(() => {
         >
           Orden # {{ order.code }}
           <OrderStatus :status="order.status" class="text-lg" />
+
+          <Popper
+            v-if="order.status === OrderStatusEnum.completed"
+            arrow
+            offsetDistance="12"
+            content="Reabrir orden"
+            :hover="true"
+            placement="top"
+            class="tooltip"
+          >
+            <div
+              class="bg-gray-100 rounded-md py-2"
+              @click="modalActive = true"
+            >
+              <RefreshCcw01Icon
+                class="h-6 w-6 mx-2 cursor-pointer text-blue-600"
+              />
+            </div>
+          </Popper>
+          <Popper
+            v-if="
+              order.status === OrderStatusEnum.pending ||
+              order.status === OrderStatusEnum.inprogress
+            "
+            arrow
+            offsetDistance="12"
+            content="Anular orden"
+            :hover="true"
+            placement="top"
+            class="tooltip"
+          >
+            <div class="bg-gray-100 rounded-md py-2">
+              <Trash01Icon class="h-6 w-6 mx-2 cursor-pointer text-red-600" />
+            </div>
+          </Popper>
         </p>
       </div>
+      <OrderCycle :orderCycle="order.orderCycle" />
     </div>
     <div class="w-full bg-white rounded-lg shadow-lg p-5 flex gap-5">
       <div class="flex items-center gap-5 w-1/3">
@@ -128,14 +127,6 @@ onUnmounted(() => {
       </div>
       <div class="w-full">
         <div class="flex items-center gap-5">
-          <div class="rounded-full h-24 w-24 bg-white">
-            <img
-              v-if="photo.imageBase64.value"
-              class="rounded-full object-cover h-full w-full"
-              :src="photo.imageBase64.value"
-              alt="Foto del paciente"
-            />
-          </div>
           <div class="flex flex-col">
             <span class="font-semibold">PACIENTE </span>
             <span class="font-semibold">Nombre </span>
@@ -167,7 +158,13 @@ onUnmounted(() => {
               <th class="text-left">Código</th>
               <th class="text-left">Nombre</th>
               <th class="text-left">Estado</th>
-              <th class="text-left"></th>
+              <th
+                class="text-left"
+                v-if="
+                  order.status === OrderStatusEnum.pending ||
+                  order.status === OrderStatusEnum.inprogress
+                "
+              ></th>
             </tr>
           </thead>
           <tbody>
@@ -187,38 +184,30 @@ onUnmounted(() => {
                   <OrderStatus :status="service.status" class="text-lg"
                 /></span>
               </td>
-              <td>
-                <span
-                  class="text-sm"
-                  v-if="order.status == OrderStatusEnum.completed"
+              <td
+                v-if="
+                  order.status === OrderStatusEnum.pending ||
+                  order.status === OrderStatusEnum.inprogress
+                "
+              >
+                <Popper
+                  v-if="
+                    service.status === OrderStatusEnum.pending ||
+                    service.status === OrderStatusEnum.inprogress
+                  "
+                  arrow
+                  offsetDistance="12"
+                  content="Anular Servicio"
+                  :hover="true"
+                  placement="top"
+                  class="tooltip"
                 >
-                  <ul v-if="service.showForContract">
-                    <li v-for="exam in service.visibleExams">
-                      <span
-                        v-if="exam.requireCertificate"
-                        href=""
-                        class="text-blue-800 flex gap-2 cursor-pointer hover:resize"
-                        @click="handleDownloadExamCertificate(service.id, exam)"
-                      >
-                        <ArrowRightIcon class="h-4 w-4 mx-2" />
-                        {{ exam.code }} - {{ exam.name }}
-
-                        <Loading01Icon
-                          v-if="exam.loading"
-                          class="animate-spin"
-                        />
-                      </span>
-                      <span
-                        v-else
-                        class="text-blue-800 flex gap-2 cursor-pointer hover:resize"
-                        @click="handleShowAnnotation(service.id, exam)"
-                      >
-                        <ArrowRightIcon class="h-4 w-4 mx-2" />
-                        {{ exam.code }} - {{ exam.name }}
-                      </span>
-                    </li>
-                  </ul>
-                </span>
+                  <div class="bg-gray-100 rounded-md py-2">
+                    <Trash01Icon
+                      class="h-6 w-6 mx-2 cursor-pointer text-red-600"
+                    />
+                  </div>
+                </Popper>
               </td>
             </tr>
           </tbody>
@@ -235,20 +224,29 @@ onUnmounted(() => {
       v-show="modalActive"
       class="fixed w-screen h-screen p-10 inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
     >
-      <div class="relative w-full h-full bg-white rounded-2xl p-5">
+      <div class="relative w-1/6 bg-white rounded-2xl p-5">
         <div
           class="absolute -right-5 -top-5 cursor-pointer rounded-full bg-gray-50 text-gray-500 p-3"
           @click="closeModal"
         >
           <XIcon class="w-6 h-6 cursor-pointer" />
         </div>
-        <div class="w-full h-full">
-          <iframe
-            :src="selectedAnnotationUrl"
-            frameborder="0"
-            class="w-full h-full"
-            style="height: 80vh"
-          ></iframe>
+        <div class="w-full h-full flex flex-col items-center justify-center">
+          <div>
+            <span class="text-lg font-semibold">Reabrir Orden</span>
+          </div>
+          <hr />
+          <div class="text-sm text-gray-500">
+            ¿Está seguro de que desea reabrir la orden?
+          </div>
+          <div class="flex gap-3 mt-5">
+            <DBtn @click="closeModal" class="bg-gray-300 hover:bg-gray-400">
+              Cancelar
+            </DBtn>
+            <DBtn class="bg-blue-600 hover:bg-blue-700 text-white">
+              Reabrir
+            </DBtn>
+          </div>
         </div>
       </div>
     </div>
