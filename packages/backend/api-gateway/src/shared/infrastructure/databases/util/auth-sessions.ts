9e90a410ec53b5c606b1370e944298c7603e09fe
Deiby Gomez <gokaju23@gmail.com>
2025-10-06 17:19:55 -0500

    feat(AuthSessions): implement session expiration cleanup and remove debug logs

--- packages/backend/api-gateway/src/shared/infrastructure/databases/util/auth-sessions.ts
+++ packages/backend/api-gateway/src/shared/infrastructure/databases/util/auth-sessions.ts
@@ -1,3 +1,5 @@
+import { set } from 'zod/v4'
+
 export class AuthSessions {
   sessions = new Map<
     string,
@@ -8,7 +10,17 @@ export class AuthSessions {
     }
   >()
   static instance: AuthSessions = new AuthSessions()
-  private constructor() {}
+
+  private constructor() {
+    setInterval(() => {
+      //clean expired sessions every 1 minutes
+      this.sessions.forEach((session, token) => {
+        if (this.isExpired(session)) {
+          this.sessions.delete(token)
+        }
+      })
+    }, 60 * 1000) //1 minutes
+  }
 
   get instance(): AuthSessions {
     if (!AuthSessions.instance) {
@@ -18,7 +30,6 @@ export class AuthSessions {
   }
 
   add(token: string, userId: string, username: string): void {
-    console.log('add', token, userId, username)
     this.sessions.set(token, {
       userId,
       username,
@@ -32,18 +43,21 @@ export class AuthSessions {
       'sessions',
       Array.from(this.sessions.entries()).map(([k]) => k)
     )
-    console.log('session', session)
-    if (
-      !session ||
-      !session.createdAt ||
-      new Date().getTime() - session.createdAt.getTime() > 1000 * 60 * 60 // 1 hour
-    ) {
+    if (!session || this.isExpired(session)) {
+      this.delete(token)
       return false
     }
     //refresh time to the session
     session.createdAt = new Date()
     this.sessions.set(token, session)
     return true
+  }
+
+  private isExpired(session: { createdAt: Date }): boolean {
+    const now = new Date()
+    const diff = now.getTime() - session.createdAt.getTime()
+    const diffInMinutes = diff / (1000 * 60)
+    return diffInMinutes > 60 // 60 minutes
   }
 
   get(token: string): string {
